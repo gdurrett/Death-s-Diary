7ef5ce9c299a0ec8b2d1c4639ed4ede0672b3036
Greg Durrett <gdurrett@gregs-imac.local>
2018-05-08 13:13:53 -0700

    Initial Commit

---
+++ Death's Diary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Death's Diary
+//
+//  Created by Greg Durrett on 5/8/18.
+//  Copyright © 2018 AppHazard Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
